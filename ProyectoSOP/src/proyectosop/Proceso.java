979763898e75f6c6ab60c465783cacb6ca7bc9fb
Diego Aguilera <diegoaguilera4@icloud.com>
2021-06-27 19:39:57 -0400

    Clase proceso

---
+++ ProyectoSOP/src/proyectosop/Proceso.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectosop;
+
+/**
+ *
+ * @author Diego Aguilera
+ */
+public class Proceso {
+    String nombre;
+    int prioridad;
+    int tamanio;
+
+    public Proceso(String nombre, int prioridad, int tamanio) {
+        this.nombre = nombre;
+        this.prioridad = prioridad;
+        this.tamanio = tamanio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(int prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public int getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(int tamanio) {
+        this.tamanio = tamanio;
+    }
+    
+}
